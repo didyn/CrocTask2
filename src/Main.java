f198d026c71f0dc362fa32d01239c6a658f00af6
didyn <g.dianka15@gmail.com>
2020-03-03 03:20:08 +0300

    pointByFind2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ public class Main {
         Annotation[] annotations = new Annotation[]{new Annotation(new BoundingBox(2,4,5,6), "прямоугольник"),
                 new Annotation(new Circle(6,7,2), "окружность1"), new Annotation(new Circle(9,10,3), "окружность2"),
                 new Annotation(new BoundingBox(2,1,4,4), "прямоугольник2")};
-        System.out.println(findByPoint(annotations, 3,1).getCaption());
+        if (findByPoint(annotations, 5, 7) == null)
+            System.out.println("not figure");
+        else System.out.println(findByPoint(annotations, 5,7).getCaption());
         BoundingBox boundingBox = new BoundingBox(4,2,-5,3);
         Circle circle = new Circle(2,6,1);
         boundingBox.move(4, 1);
@@ -23,10 +25,9 @@ public class Main {
                         && ((BoundingBox) annotations[i].getFigure()).getY2() >= y)
                     return annotations[i];
             if (annotations[i].getFigure() instanceof Circle)
-                if (((Circle) annotations[i].getFigure()).getX() + ((Circle) annotations[i].getFigure()).getR() >= x
-                        && ((Circle) annotations[i].getFigure()).getX() - ((Circle) annotations[i].getFigure()).getR() <= x
-                        && ((Circle) annotations[i].getFigure()).getY() + ((Circle) annotations[i].getFigure()).getR() >= y
-                        &&((Circle) annotations[i].getFigure()).getY() - ((Circle) annotations[i].getFigure()).getR() <= y)
+                if (Math.pow((x - ((Circle) annotations[i].getFigure()).getX()), 2) +
+                        Math.pow((y - ((Circle) annotations[i].getFigure()).getY()), 2) <=
+                        Math.pow(((Circle) annotations[i].getFigure()).getR(), 2))
                     return annotations[i];
         }
         return null;
